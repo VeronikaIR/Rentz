4b7daf9dcabc6a24a12555dbd65f037e1761a4ab
Veronika Racheva <veronika.racheva@mail.schwarz>
2023-11-29 22:41:10 +0200

    add authenticationService

--- Rentz-Frontend/new-proj/src/app/login/login/login.component.ts
+++ Rentz-Frontend/new-proj/src/app/login/login/login.component.ts
@@ -1,7 +1,5 @@
-
-import {User} from "../models/user";
-import {AuthService} from "../service/auth.service";
-import {Component, Input, OnInit} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -9,18 +7,63 @@ import {Component, Input, OnInit} from "@angular/core";
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  // public user: User = {
-  //   id: 1,
-  //   name: 'John Doe',
-  //   email: 'johndoe@example.com'
-  // };
 
-  @Input() userId: number = 1;
-  @Input() username: string = 'John Doe';
-  @Input() userEmail: string = 'johndoe@example.com';
-  constructor(private authService: AuthService) { }
+
+  showRegisterForm: boolean = false;
+  loginForm!: FormGroup;
+  isLoggingIn = false;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+  }
 
   ngOnInit() {
-    //this.user = this.authService.getUser();
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
+
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Handle login logic here
+      console.log('Login successful!', this.loginForm.value);
+    } else {
+      // Form is invalid, mark fields as touched to display validation errors
+      this.loginForm.markAllAsTouched();
+    }
+  }
+
+
+  login() {
+    this.isLoggingIn = true;
+
+    this.authenticationService.signIn({
+      email: this.form.value.email,
+      password: this.form.value.password
+    }).subscribe({
+      next: () => this.router.navigate(['home']),
+      error: error => {
+        this.isLoggingIn = false;
+        this.snackBar.open(error.message, "OK", {
+          duration: 5000
+        })
+      }
+    });
+  }
+
+
+  toggleRegister() {
+    this.showRegisterForm = !this.showRegisterForm;
+  }
+
+
+  loginWithGoogle() {
+    // Implement Google login logic here
+    console.log('Logging in with Google');
+    // You may want to use a service for Google authentication or an external library.
+  }
+
 }
--- Rentz-Frontend/new-proj/src/app/login/service/auth.service.ts
+++ Rentz-Frontend/new-proj/src/app/login/service/auth.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
 import {User} from "../models/user";
-import {catchError, Observable, of} from "rxjs";
+import {catchError, from, Observable, of, throwError} from "rxjs";
 import {HttpClient} from "@angular/common/http";
+import {AngularFireAuth} from "@angular/fire/compat/auth";
 
 
 @Injectable({
@@ -11,7 +12,7 @@ export class AuthService {
 
   private apiUrl = 'http://localhost:8080/api/users';
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private auth: AngularFireAuth) {
   }
 
   getUserById(userId: string): Observable<User | null> {
@@ -22,4 +23,44 @@ export class AuthService {
       })
     );
   }
+
+  signIn(params: SignIn): Observable<any> {
+    return from(this.auth.signInWithEmailAndPassword(
+      params.email, params.password
+    )).pipe(
+      catchError((error: FirebaseError) =>
+        throwError(() => new Error(this.translateFirebaseErrorMessage(error)))
+      )
+    );
+  }
+
+  recoverPassword(email: string): Observable<void> {
+    return from(this.auth.sendPasswordResetEmail(email)).pipe(
+      catchError((error: FirebaseError) =>
+        throwError(() => new Error(this.translateFirebaseErrorMessage(error)))
+      )
+    );
+  }
+
+  private translateFirebaseErrorMessage({code, message}: FirebaseError) {
+    if (code === "auth/user-not-found") {
+      return "User not found.";
+    }
+    if (code === "auth/wrong-password") {
+      return "User not found.";
+    }
+    return message;
+  }
+
+}
+
+type SignIn = {
+  email: string;
+  password: string;
+}
+
+type FirebaseError = {
+  code: string;
+  message: string
+}
 }
